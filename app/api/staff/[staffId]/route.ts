a26a50e8655d14676ee28dd34848b012ed689d80
reisy1999 <abereneibu@gmail.com>
2025-12-06 19:42:34 +0900

    職員idもらって情報を返すapi

--- app/api/staff/[staffId]/route.ts
+++ app/api/staff/[staffId]/route.ts
@@ -3,7 +3,7 @@ import { PrismaClient } from '@prisma/client'
 // クラスからインスタンス作成するobj
 const prisma = new PrismaClient()
 
-//職員idが主キー
+//職員idもらって職員情報を返す
 export async function GET(
     request:Request,
     { params }: {params: {staffId: string}}
@@ -11,4 +11,8 @@ export async function GET(
     const staff = await prisma.staff.findUnique({
         where: { staffId: params.staffId }
     })
+    if ( staff === null ){
+        return Response.json({error: "職員情報が未登録です" }, {status: 404})
+    }
+        return Response.json(staff)
 }
